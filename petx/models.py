703f532aa2025de943f33d4030832eac1ab9375d
Rafael Alexandre Cascardo <rafael.a.cascardo.pl@gmail.com>
2026-09-03 21:32:10 -0300

    models para perfil pet completo

--- petx/models.py
+++ petx/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+
+
+class Raca(models.Model): 
+    raca = models.CharField(max_length = 100, unique = True)
+    
+    def __str__(self):
+        return self.name
+    
+class Porte(models.Model): 
+    porte = models.CharField(max_length = 100, unique = True)
+    
+    def __str__(self):
+        return self.name
+    
+class Cor(models.Model): 
+    cor = models.CharField(max_length = 100, unique = True)
+    
+    def __str__(self):
+        return self.name
+
+class Pet(models.Model): 
+    nome = models.CharField(max_length=15)
+    raca = models.ManyToManyField(Raca, related_name="pets", blank=True)
+    castrado = models.BooleanField(default=False)
+    vacinado = models.BooleanField(default=False)
+    nascimento = models.DateField(default=date.today)
+    descricao = models.CharField(max_length=1500)
+    class Sexo(models.TextChoices):
+        MACHO = "M", "Macho"
+        FEMEA = "F", "Femea"
+    sexo = models.CharField(max_length = 1, choices = Sexo.choices)
+    porte = models.ManyToManyField(Porte, related_name="pets", blank=True)
+    cor = models.ManyToManyField(Cor, related_name="pets", blank=True)
+    
+
+
+
+
+    
+    
