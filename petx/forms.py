7b37b657524da581da74ac54ac3ee854b254fce3
Rafael Alexandre Cascardo <rafael.a.cascardo.pl@gmail.com>
2026-09-08 21:28:23 -0300

    forms -R

--- petx/forms.py
+++ petx/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Pet, Raca, Cor, Porte, Perfil, instituicao, Anuncio, Tutor, Adotante
+from .models import Pet, Raca, Cor, Porte, Perfil, instituicao, Anuncio, Tutor, Adotantemodels
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
